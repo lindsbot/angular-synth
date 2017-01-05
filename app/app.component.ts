3f50a1bbf483d278305b479d1e1c7a6f6898910a
lindsbot <lindsay@modeanalytics.com>
2017-01-05 13:51:22 -0800

    Add note data

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,28 @@
+
+import { isAsciiLetter } from '@angular/compiler/src/chars';
 import { Component } from '@angular/core';
+
+import { CHIMES_1 } from './chimes.data';
 
 @Component({
   selector: 'angular-synth',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <div>
+      <ul>
+        <li *ngFor="let chime of chimes">
+          <div [ngStyle]="{
+            'background-color': chime.color
+          }">
+          {{chime.note}}
+          </div>
+        </li>
+      </ul>
+    </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent  {
+  name = 'Angular';
+  chimes = CHIMES_1;
+}
+
---
+++ app/chimes.data.ts
@@ -0,0 +1,68 @@
+class SynthButton {
+  id: number;
+  color: string;
+  note: string;
+}
+
+export const CHIMES_1: SynthButton[] = [
+  // RUBY
+  {
+    id: 1,
+    color: '#D81159',
+    note: 'G3'
+  },
+  // DARK RASPBERRY
+  {
+    id: 2,
+    color: '#8F2D56',
+    note: 'A3'
+  },
+  // CELADON GREEN
+  {
+    id: 3,
+    color: '#218380',
+    note: 'B3'
+  },
+  // YELLOW ORANGE
+  {
+    id: 4,
+    color: '#FBB13C',
+    note: 'C4'
+  },
+  // MIDDLE BLUE
+  {
+    id: 5,
+    color: '#73D2DE',
+    note: 'D4'
+  },
+  // ROSE
+  {
+    id: 6,
+    color: '#FF0E82',
+    note: 'G4'
+  },
+  // MAXIMUM RED PURPLE
+  {
+    id: 7,
+    color: '#A93784',
+    note: 'A4'
+  },
+  // AMAZONITE
+  {
+    id: 8,
+    color: '#00C9A7',
+    note: 'B4'
+  },
+  // MUSTARD
+  {
+    id: 9,
+    color: '#FFDE63',
+    note: 'C5'
+  },
+  // ELECTRIC BLUE
+  {
+    id: 10,
+    color: '#87FFFF',
+    note: 'D5'
+  }
+]
